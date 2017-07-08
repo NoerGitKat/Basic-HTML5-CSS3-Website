1ed0735b26a8d42056b6355f994857b30e75bcbb
Noer <noerpaanakker@hotmail.com>
2017-07-08 20:59:09 +0200

    created about and services page

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
+app.get('/services', (req, res) => {
+	res.render('services');
+});
+
 const server = app.listen(3000, () => {
 	console.log('The server has started at port:' + server.address().port);
 });
